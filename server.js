28f0479ffc66c0ee854fcd5cf27bf3a41e1da648
wladok <125205232+wladok@users.noreply.github.com>
2026-06-10 20:04:01 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const BOT_TOKEN = "7696883936:AAEpFLtAUg6PK2f2mzI1Z_9n54i1CZwJiZ0";
+const CHAT_ID = "1357735944";
+
+app.post("/send", async (req, res) => {
+  try {
+    const { name, phone, message } = req.body;
+
+    const text =
+`🛠️ New Request
+
+👤 Name: ${name}
+
+📞 Phone: ${phone}
+
+💬 Message:
+${message || "No description"}`;
+
+    await axios.post(
+      'https://api.telegram.org/bot${7696883936:AAEpFLtAUg6PK2f2mzI1Z_9n54i1CZwJiZ0}/sendMessage',
+      {
+        chat_id: 1357735944,
+        text: text
+      }
+    );
+
+    res.send("Application sent successfully!");
+  } catch (error) {
+    console.error(error.response?.data || error);
+
+    res.status(500).send("Error sending application");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
