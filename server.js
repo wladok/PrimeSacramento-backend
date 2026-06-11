15746721f30c49d944dac61afa0ec25f3dc203f7
wladok <125205232+wladok@users.noreply.github.com>
2026-06-10 21:45:00 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const multer = require("multer");
-const upload = multer();
-
-
+const FormData = require("form-data");
+const fs = require("fs");
 
 const app = express();
+const upload = multer();
 
 app.use(cors());
 app.use(express.json());
@@ -17,36 +17,50 @@ const CHAT_ID = "1357735944";
 
 app.post("/send", upload.single("photo"), async (req, res) => {
   try {
-    console.log("BODY:", req.body);
-
     const { name, phone, message } = req.body || {};
+    const photo = req.file;
 
     const text =
 `🛠️ New Request
 
 👤 Name: ${name}
-
 📞 Phone: ${phone}
 
 💬 Message:
 ${message || "No description"}`;
 
-    await axios.post(
-      `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
-      {
-        chat_id: CHAT_ID,
-        text: text
-      }
-    );
+    if (photo) {
+      const form = new FormData();
+
+      form.append("chat_id", CHAT_ID);
+      form.append("caption", text);
+      form.append("photo", fs.createReadStream(photo.path));
+
+      await axios.post(
+        `https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto`,
+        form,
+        {
+          headers: form.getHeaders(),
+        }
+      );
+    } else {
+      await axios.post(
+        `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
+        {
+          chat_id: CHAT_ID,
+          text: text
+        }
+      );
+    }
 
     res.send("Application sent successfully!");
   } catch (error) {
     console.error(error.response?.data || error);
-
     res.status(500).send("Error sending application");
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT);
 });
